8c244151922c91682868902bfef166c3e093a8a3
LudwigPeschik <124577705+LudwigPeschik@users.noreply.github.com>
2023-02-14 17:31:35 +0100

    Update to IRFModel and Energy Dependance

--- Eff_area/energy_independent/Dataset_Creation.py
+++ Eff_area/energy_independent/Dataset_Creation.py
@@ -5,13 +5,16 @@ from gammapy.modeling.models import (
     PowerLawSpectralModel,
     ExpCutoffPowerLawSpectralModel,
     SkyModel,
-    PowerLawNuisanceSpectralModel,
-    PowerLawNormNuisanceSpectralModel,
-    ExpCutoffPowerLawNuisanceSpectralModel,
+    #PowerLawNuisanceSpectralModel,
+    #PowerLawNormNuisanceSpectralModel,
+    #ExpCutoffPowerLawNuisanceSpectralModel,
     GaussianSpatialModel)
 #from MapDatasetNuisanceE import MapDatasetNuisanceE
 from gammapy.modeling import Parameter, Parameters
 from gammapy.datasets import MapDataset
+
+from gammapy.modeling.models import SpectralModel
+from gammapy.modeling.models.cube import IRFModel
 
 path_crab = '/home/hpc/caph/mppi045h/3D_analysis/N_parameters_in_L/nuisance_summary/Crab'
 
@@ -21,10 +24,12 @@ path_crab = '/home/hpc/caph/mppi045h/3D_analysis/N_parameters_in_L/nuisance_summ
 class sys_dataset():
     def __init__(self, 
                  dataset_asimov,
-                 factor,
+                 shift,
+                 tilt,
                  rnd):
         self.dataset_asimov = dataset_asimov
-        self.factor = factor
+        self.shift = shift
+        self.tilt = tilt
         self.rnd = rnd
         
     def set_model(self):
@@ -47,53 +52,32 @@ class sys_dataset():
     
     def create_dataset(self):
         dataset = self.dataset_asimov.copy()
-        exposure = dataset.exposure.copy()
-        exposure.data *= (1-self.factor)
-        background = dataset.background.copy()
-        #background.data *= (1-self.factor)
-        dataset.exposure = exposure
-        dataset.background = background
-        if self.rnd:
-            counts_data = np.random.poisson(self.dataset_asimov.counts.data)
-        else:
-            counts_data = self.dataset_asimov.counts.data
-
-        dataset.counts.data = counts_data
         models = self.set_model()
+        #bkg model
         bkg_model = FoVBackgroundModel(dataset_name=dataset.name)
         bkg_model.parameters['tilt'].frozen  = False
+        models.append(bkg_model)
+        #irf model
+        IRFmodel = IRFModel(dataset_name = dataset.name)
+        IRFmodel.parameters['tilt_nuisance'].frozen  = False
+        models.append(IRFmodel)
+        
+        dataset.models = models
+        dataset.models.parameters['norm_nuisance'].value  = self.shift
+        dataset.models.parameters['tilt_nuisance'].value  = self.tilt
+        
+        if self.rnd:
+            counts_data = np.random.poisson(dataset.npred().data)
+        else:
+            counts_data = dataset.npred().data
+
+        dataset.counts.data = counts_data
+        # set models without the IRF model
+        models = self.set_model()
         models.append(bkg_model)
         dataset.models = models
         return dataset
     
-    
-    def set_model_N(self):
-        model_spatial = GaussianSpatialModel(
-            lon_0="83.631 deg",
-            lat_0="22.018 deg",
-            sigma="0.02 deg",
-        )
-        model_spectrum  = ExpCutoffPowerLawNuisanceSpectralModel(
-            index=2.3,
-            index_nuisance = 0,
-            amplitude="1e-12 TeV-1 cm-2 s-1",  
-            amplitude_nuisance = 0,
-            lambda_ =  "0.1 TeV-1",
-            E_nuisance = 0)
-
-        #print(len(model_spectrum.parameters))
-        #print(len(model_spectrum.default_parameters))
-
-        source_model = SkyModel(spatial_model = model_spatial,
-                               spectral_model = model_spectrum,
-                               name = "SourceN")  
-        source_model.parameters['lon_0'].frozen = True
-        source_model.parameters['lat_0'].frozen = True
-        models = Models(source_model)
-        return models
-
-
-
     def create_dataset_N(self):
         dataset_ = self.create_dataset()
         dataset_N = MapDataset(
@@ -105,15 +89,14 @@ class sys_dataset():
                 mask_safe=dataset_.mask_safe.copy(),
                 gti=dataset_.gti.copy(),
                 name='dataset N')
-        models = self.set_model_N()
-        #bkg_spectralmodel = PowerLawNormNuisanceSpectralModel(
-        #            tilt=1,
-        #            norm=1,
-        #            norm_nuisance=0
-        #)
-        bkg_model = FoVBackgroundModel(dataset_name=dataset_N.name,
-                                      )#spectral_model = bkg_spectralmodel)
+        models = self.set_model()
+        #bkg model
+        bkg_model = FoVBackgroundModel(dataset_name=dataset_N.name)
         bkg_model.parameters['tilt'].frozen  = False
         models.append(bkg_model)
+        #irf model
+        IRFmodel = IRFModel(dataset_name = dataset_N.name)
+        IRFmodel.parameters['tilt_nuisance'].frozen  = False
+        models.append(IRFmodel)
         dataset_N.models = models
         return dataset_N
